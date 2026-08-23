d49a243719e882009d2dbf94d1bf235faeab8a8b
brian <bwoodson1972@gmail.com>
2026-08-23 13:29:47 -0500

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
+import tailwindcss from "@tailwindcss/vite";
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import svelte from '@astrojs/svelte';
+
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [svelte()],
+
+  vite: {
+      plugins: [tailwindcss()],
+	},
+
+  adapter: vercel(),
+});
